ba49f363c89d80ee7d9ee92b36e3427360f37931
AlexUA_win <alex2011ua@gmail.com>
2023-03-02 12:14:43 +0100

    add main project and models

--- isi_backend/isi_backend/settings.py
+++ isi_backend/isi_backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # installed apps
     'rest_framework',
-    'rest_framework_jwt',
+    'rest_framework_simplejwt',
     # apps
     "chat",
 ]
